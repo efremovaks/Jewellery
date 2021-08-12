bd32447e82454686e7cfaaa07d9e01de2bfa50fc
kseniya <efremovaks@gmail.com>
2021-08-12 12:39:24 +0300

    catalog

--- build/js/script.js
+++ build/js/script.js
@@ -33,6 +33,56 @@ accordionItems.forEach(function (item, index) {
 
 'use strict';
 
+// Filter
+
+var filter = document.querySelector('.catalog__filter');
+
+if (filter) {
+  var filterOpen = filter.querySelector('.catalog__filter-header');
+  var filterClose = filter.querySelector('.catalog__filter-close');
+}
+
+if (filter) {
+
+  if (filter.classList.contains('catalog__filter--nojs')) {
+    filter.classList.remove('catalog__filter--nojs');
+  }
+
+  filterOpen.addEventListener('click', function () {
+    if (filter.classList.contains('catalog__filter-closed')) {
+      filter.classList.remove('catalog__filter-closed');
+      filter.classList.add('catalog__filter-open');
+
+    } else {
+      filter.classList.add('catalog__filter-closed');
+      filter.classList.remove('catalog__filter-open');
+    }
+  });
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      if (filter.classList.contains('catalog__filter-open')) {
+        evt.preventDefault();
+        filter.classList.remove('catalog__filter-open');
+        filter.classList.add('catalog__filter-closed');
+      }
+    }
+  });
+
+  filterClose.addEventListener('click', function () {
+    if (filter.classList.contains('catalog__filter-closed')) {
+      filter.classList.remove('catalog__filter-closed');
+      filter.classList.add('catalog__filter-open');
+
+    } else {
+      filter.classList.add('catalog__filter-closed');
+      filter.classList.remove('catalog__filter-open');
+    }
+  });
+}
+
+'use strict';
+
 // menu
 
 // var body = document.querySelector('.page-body');
